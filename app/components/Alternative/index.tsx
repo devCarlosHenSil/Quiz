4d83d022ebc4b7ac37f19d1f9972e3b423565a2c
devCarlosHenSil <sh.carlo.henrique@gmail.com>
2024-10-25 15:33:59 -0300

    adição de comentários

--- app/components/Alternative/index.tsx
+++ app/components/Alternative/index.tsx
@@ -7,7 +7,7 @@ interface AlternativeProps {
 
 export function Alternative(props: AlternativeProps) {
   const id = `alternative-${props.order}`;
-  // Fazer com que ao apertar ENTER ou Espaço a alternativa seja selecionada
+  // A Fazer com que ao apertar ENTER ou Espaço a alternativa seja selecionada
   return (
     <>
       <input
